8b97bf069fc58cb20c97b5196c4212336902a62d
YLabz Developer <developer@ylabz.com>
2023-02-17 20:41:32 -0800

    add skeleton for core-domain and core-network

---
+++ core-domain/build.gradle.kts
@@ -0,0 +1,44 @@
+@Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.kapt)
+}
+
+android {
+    namespace = "com.test.fitnessstudios.core.domain"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildFeatures {
+        aidl = false
+        buildConfig = false
+        renderScript = false
+        shaders = false
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Arch Components
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
---
+++ core-domain/src/main/java/com/test/fitnessstudios/core/domain/GetGymUseCase.kt
@@ -0,0 +1,10 @@
+package com.test.fitnessstudios.core.domain
+
+import javax.inject.Inject
+
+class GetGymUseCase @Inject constructor(
+    //private val newsRepository: NewsRepository,
+    //private val userDataRepository: UserDataRepository,
+) {
+
+}

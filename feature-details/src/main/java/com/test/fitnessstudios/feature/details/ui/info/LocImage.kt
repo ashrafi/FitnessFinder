ebc77156c4f8f7e04943bba740680f43c1354eb7
YLabz Developer <developer@ylabz.com>
2023-05-20 16:22:06 -0700

    Next round of making Detail screen pretty by updating the top image

--- feature-details/src/main/java/com/test/fitnessstudios/feature/details/ui/info/LocImage.kt
+++ feature-details/src/main/java/com/test/fitnessstudios/feature/details/ui/info/LocImage.kt
@@ -1,19 +1,28 @@
 package com.test.fitnessstudios.feature.details.ui.info
 
 import android.content.Intent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.test.fitnessstudios.feature.details.R
 
@@ -26,11 +35,12 @@ internal fun LocImg(
 ) {
     val sendIntent: Intent = Intent().apply {
         action = Intent.ACTION_SEND
-        putExtra(Intent.EXTRA_TEXT, "Web Site:\n${webURL}")
+        putExtra(Intent.EXTRA_TEXT, "Web Site:\n$webURL")
         type = "text/plain"
     }
     val shareIntent = Intent.createChooser(sendIntent, null)
     val context = LocalContext.current
+
     Box {
         photoURL?.let { photo ->
             AsyncImage(
@@ -42,15 +52,42 @@ internal fun LocImg(
                 contentScale = ContentScale.Fit
             )
         }
-        Button(onClick = {
-            context.startActivity(shareIntent)
-        }) {
-            Icon(painterResource(R.drawable.ic_share), "share")
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .background(Color.Black.copy(alpha = 0.5f))
+                .padding(8.dp)
+        ) {
+            Text(
+                text = name,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                ),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
-        Text("${name}")
+
+        Button(
+            onClick = {
+                context.startActivity(shareIntent)
+            },
+            modifier = Modifier.align(Alignment.TopEnd)
+        ) {
+            Icon(
+                painterResource(R.drawable.ic_share),
+                contentDescription = "Share"
+            )
+        }
     }
-    //Log.d("GraphQL", "this is it ${it.name}")
 }
+
+
+
 
 @Preview
 @Composable
